9ead10a3869bc5cdaf2cf1f5c795b7fab7665028
TrojanNikita <nikitatrojan@mail.ru>
2019-12-16 15:44:45 +0300

    with mysql

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject._
 
-import play.api.libs.json.Json
+import play.api.libs.json._
 import daos.TodosDAO
 import play.api._
 import play.api.mvc._
@@ -11,6 +11,7 @@ import play.api.i18n._
 import play.api.data.Form
 import play.api.data.Forms.{  mapping, nonEmptyText }
 import scala.concurrent.{ExecutionContext,Future}
+import models.Todo
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -28,18 +29,18 @@ class HomeController @Inject()(repo: TodosDAO,
   /**
    * The mapping for the todo form.
    */
-  val todoForm: Form[CreateTodoForm] = Form {
-    mapping(
-      "name" -> nonEmptyText
-    )(CreateTodoForm.apply)(CreateTodoForm.unapply)
-  }
+  // val todoForm: Form[CreateTodoForm] = Form {
+  //   mapping(
+  //     "name" -> nonEmptyText
+  //   )(CreateTodoForm.apply)(CreateTodoForm.unapply)
+  // }
 
   /**
    * The index action.
    */
-  def index = Action { implicit request =>
-    Ok(views.html.index(todoForm))
-  }
+  // def index = Action { implicit request =>
+  //   Ok(views.html.index(todoForm))
+  // }
 
 
 
@@ -48,23 +49,48 @@ class HomeController @Inject()(repo: TodosDAO,
    *
    * This is asynchronous, since we're invoking the asynchronous methods on PersonRepository.
    */
-  def addTodo = Action.async { implicit request =>
-    // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle succes.
-    todoForm.bindFromRequest.fold(
-      // The error function. We return the index page with the error form, which will render the errors.
-      // We also wrap the result in a successful future, since this action is synchronous, but we're required to return
-      // a future because the person creation function returns a future.
-      errorForm => {
-        Future.successful(Ok(views.html.index(errorForm)))
-      },
-      // There were no errors in the from, so create the todo.
-      todo => {
-        repo.create(todo.name).map { _ =>
-          // If successful, we simply redirect to the index page.
-          Redirect(routes.HomeController.index).flashing("success" -> "todo.created")
-        }
-      }
-    )
+  // def addTodo = Action(parse.json) { implicit request =>
+  //   // // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle succes.
+  //   // todoForm.bindFromRequest.fold(
+  //   //   // The error function. We return the index page with the error form, which will render the errors.
+  //   //   // We also wrap the result in a successful future, since this action is synchronous, but we're required to return
+  //   //   // a future because the person creation function returns a future.
+  //   //   errorForm => {
+  //   //     Future.successful(Ok(views.html.index(errorForm)))
+  //   //   },
+  //   //   // There were no errors in the from, so create the todo.
+  //   //   todo => {
+  //   //     repo.create(todo.name).map { _ =>
+  //   //       // If successful, we simply redirect to the index page.
+  //   //       Redirect(routes.HomeController.index).flashing("success" -> "todo.created")
+  //   //     }
+  //   //   }
+  //   // )
+  //   val newTodo = request.body.as[String]
+
+  //   val todos = repo.create(newTodo)
+  //   Ok(Json.toJson(todos))
+
+
+  // }
+
+
+  def addTodo = Action(parse.tolerantJson) { implicit request =>
+
+
+      val name = request.body.as[Todo]
+
+      
+      // Ok(Json.toJson(name))
+
+      //val todos = repo.create(name)
+
+      Ok(Json.toJson(name))      
+
+    // val todos = repo.create(name)
+    // Ok(Json.toJson(todos))
+
+
   }
 
   def getTodo(id:Long) = Action.async { implicit request =>
@@ -101,13 +127,6 @@ class HomeController @Inject()(repo: TodosDAO,
 
 
 
-
-
-
-
-
-
-
   /**
    * A REST endpoint that gets all the people as JSON.
    */
@@ -119,4 +138,4 @@ class HomeController @Inject()(repo: TodosDAO,
 
 }
 
-case class CreateTodoForm(name: String)
+// case class CreateTodoForm(name: String)
--- build.sbt
+++ build.sbt
@@ -1,40 +1,48 @@
-name := """[AЕеЕкщофт1"""
-organization := "ффф"
+// name := """[AЕеЕкщофт1"""
+// organization := "ффф"
 
-version := "1.0-SNAPSHOT"
+// version := "1.0-SNAPSHOT"
 
-lazy val root = (project in file(".")).enablePlugins(PlayScala)
+// lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.13.1"
+// scalaVersion := "2.13.1"
 
-libraryDependencies += guice
-libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % Test
-
-//добавляем play-json
-//libraryDependencies += "com.typesafe.play" %% "play-json" % "2.7.3"
-
-//для работы с БД
-//libraryDependencies ++= Seq(
-//  "org.reactivemongo" %% "play2-reactivemongo" % "0.19.3-play27"
-//)
+// libraryDependencies += guice
+// libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % Test
 
 
-libraryDependencies ++= Seq(
-  "com.typesafe.play" %% "play-slick" % "4.0.2",
-  "com.typesafe.play" %% "play-slick-evolutions" % "4.0.2"
+// libraryDependencies ++= Seq(
+//   "com.typesafe.play" %% "play-slick" % "4.0.2",
+//   "com.typesafe.play" %% "play-slick-evolutions" % "4.0.2"
 
-)
+// )
 
-libraryDependencies+="mysql" % "mysql-connector-java" % "5.1.34"
-
-
-//libraryDependencies+="org.postgresql" % "postgresql" % "42.2.1"
+// libraryDependencies+="mysql" % "mysql-connector-java" % "8.0.18"
 
 
-libraryDependencies += "com.typesafe.play" %% "play-json" % "2.8.1"
+// //libraryDependencies+="org.postgresql" % "postgresql" % "42.2.1"
 
-// Adds additional packages into Twirl
-//TwirlKeys.templateImports += "ффф.controllers._"
 
-// Adds additional packages into conf/routes
-// play.sbt.routes.RoutesKeys.routesImport += "ффф.binders._"
+// libraryDependencies += "com.typesafe.play" %% "play-json" % "2.8.1"
+
+
+lazy val root = (project in file("."))
+  .enablePlugins(PlayScala)
+  .settings(
+    name := """[AЕеЕкщофт1""",
+    version := "2.8.x",
+    scalaVersion := "2.13.1",
+    libraryDependencies ++= Seq(
+      guice,
+      "com.typesafe.play" %% "play-slick" % "5.0.0",
+      "com.typesafe.play" %% "play-slick-evolutions" % "5.0.0",
+      "mysql" % "mysql-connector-java" % "8.0.13",
+      "com.typesafe.play" %% "play-json" % "2.8.1",
+      specs2 % Test,
+    ),
+    scalacOptions ++= Seq(
+      "-feature",
+      "-deprecation",
+      "-Xfatal-warnings"
+    )
+  )
